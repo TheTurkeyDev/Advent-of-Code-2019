2f3f3c10470d53f51440104667a90b10c3df1393
Turkey2349 <turkey2349@gmail.com>
2019-12-07 11:38:08 -0500

    Day 7 part 1 code

---
+++ src/com/theprogrammingturkey/aoc2019/Day7.java
@@ -0,0 +1,174 @@
+package com.theprogrammingturkey.aoc2019;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day7
+{
+	List<Integer[]> phaseSettings = new ArrayList<>();
+
+	public Day7()
+	{
+		generateCombintaions();
+		Scanner scanner = new Scanner(System.in);
+		String program = FileUtil.loadFile("res/day7-1.txt").get(0);
+		String[] rawStringList = program.split(",");
+		Integer[] programReset = new Integer[rawStringList.length];
+		for(int i = 0; i < rawStringList.length; i++)
+			programReset[i] = Integer.parseInt(rawStringList[i]);
+
+
+		int largestTotal = 0;
+
+		for(Integer[] phases : phaseSettings)
+		{
+			boolean phaseInput = true;
+			int total = 0;
+			for(int j = 0; j < 5; j++)
+			{
+				Integer[] programList = programReset.clone();
+
+				int pc = 0;
+				boolean run = true;
+				int[] params = new int[3];
+				boolean[] paramModes = new boolean[3];
+				while(run)
+				{
+					String instruction = String.valueOf(programList[pc]);
+					for(int i = instruction.length(); i < 5; i++)
+						instruction = "0" + instruction;
+
+					int opcode = Integer.parseInt(instruction.substring(instruction.length() - 2));
+					paramModes[0] = instruction.charAt(2) == '0';
+					paramModes[1] = instruction.charAt(1) == '0';
+					paramModes[2] = instruction.charAt(0) == '0';
+
+					int result;
+					switch(opcode)
+					{
+						case 1:
+							params[0] = getValueforParamMode(paramModes[0], programList, pc + 1);
+							params[1] = getValueforParamMode(paramModes[1], programList, pc + 2);
+							params[2] = programList[pc + 3];
+
+							result = params[0] + params[1];
+							programList[params[2]] = result;
+							pc += 4;
+							break;
+						case 2:
+							params[0] = getValueforParamMode(paramModes[0], programList, pc + 1);
+							params[1] = getValueforParamMode(paramModes[1], programList, pc + 2);
+							params[2] = programList[pc + 3];
+
+							result = params[0] * params[1];
+							programList[params[2]] = result;
+							pc += 4;
+							break;
+						case 3:
+							int input;
+							if(phaseInput)
+								input = phases[j];
+							else
+								input = total;
+							phaseInput = !phaseInput;
+
+							params[0] = programList[pc + 1];
+							programList[params[0]] = input;
+							pc += 2;
+							break;
+						case 4:
+							params[0] = getValueforParamMode(paramModes[0], programList, pc + 1);
+							total = params[0];
+							pc += 2;
+							run = false;
+							break;
+						case 5:
+							params[0] = getValueforParamMode(paramModes[0], programList, pc + 1);
+							params[1] = getValueforParamMode(paramModes[1], programList, pc + 2);
+							if(params[0] != 0)
+								pc = params[1];
+							else
+								pc += 3;
+							break;
+						case 6:
+							params[0] = getValueforParamMode(paramModes[0], programList, pc + 1);
+							params[1] = getValueforParamMode(paramModes[1], programList, pc + 2);
+							if(params[0] == 0)
+								pc = params[1];
+							else
+								pc += 3;
+							break;
+						case 7:
+							params[0] = getValueforParamMode(paramModes[0], programList, pc + 1);
+							params[1] = getValueforParamMode(paramModes[1], programList, pc + 2);
+							params[2] = programList[pc + 3];
+							programList[params[2]] = params[0] < params[1] ? 1 : 0;
+							pc += 4;
+							break;
+						case 8:
+							params[0] = getValueforParamMode(paramModes[0], programList, pc + 1);
+							params[1] = getValueforParamMode(paramModes[1], programList, pc + 2);
+							params[2] = programList[pc + 3];
+							programList[params[2]] = params[0] == params[1] ? 1 : 0;
+							pc += 4;
+							break;
+						default:
+							run = false;
+							break;
+					}
+				}
+			}
+
+			if(total > largestTotal)
+				largestTotal = total;
+		}
+
+		System.out.println(largestTotal);
+
+		scanner.close();
+	}
+
+
+	public int getValueforParamMode(boolean posmode, Integer[] program, int pc)
+	{
+		if(posmode)
+			return program[program[pc]];
+		else
+			return program[pc];
+	}
+
+	public void generateCombintaions()
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				if(i == j)
+					continue;
+				for(int k = 0; k < 5; k++)
+				{
+					if(i == k || j == k)
+						continue;
+					for(int l = 0; l < 5; l++)
+					{
+						if(i == l || j == l || k == l)
+							continue;
+						for(int m = 0; m < 5; m++)
+						{
+							if(i == m || j == m || k == m || l == m)
+								continue;
+							phaseSettings.add(new Integer[]{i, j, k, l, m});
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args)
+	{
+		new Day7();
+	}
+}
